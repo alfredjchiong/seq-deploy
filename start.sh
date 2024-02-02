ffe733b7b6f479a26d94730d6e59f23ffa4c11c2
alfredjchiong <35852224+alfredjchiong@users.noreply.github.com>
2024-02-02 11:49:43 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generar el hash de la contraseña
-PH=$(echo "$SEQ_ADMIN_PASSWORD" | docker run --rm -i datalust/seq config hash)
+#PH=$(echo "$SEQ_ADMIN_PASSWORD" | docker run --rm -i datalust/seq config hash)
 
 # Iniciar Seq con la contraseña hash
-docker run --name seq -d --restart unless-stopped -e ACCEPT_EULA=Y -e SEQ_FIRSTRUN_ADMINPASSWORDHASH="$PH" -p 8080:80 -p 5341:5341 datalust/seq
+docker run --name seq -d --restart unless-stopped -e ACCEPT_EULA=Y -p 5341:5341 datalust/seq
